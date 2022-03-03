54debd9292eae5ba854ebb99a73784886d76e27f
yangfan1122 <yangfan1122@gmail.com>
2022-03-03 18:01:41 +0800

    base

---
+++ README.md
@@ -0,0 +1,11 @@
+# tgz-checker
+检查待导入verdaccio storage内的各npm包内是否含有.tgz包  
+```
+npm i -g tgz-checker
+```
+
+### 使用
+命令行进入npm包所在目录，执行
+```
+tgz
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const path = require('path')
+const cliPath = process.cwd()
+let logContent = ''
+const timestamp = new Date().getTime()
+const logFileName = timestamp + '-tgz_debug.log'
+
+check(cliPath)
+
+function check (directory) {
+  const folders = fs.readdirSync(directory)
+  for (let key in folders) {
+    const fullPath = directory + path.sep + folders[key]
+    if (fs.lstatSync(fullPath).isDirectory()) {
+      check(fullPath)
+    } else if (path.basename(fullPath) === 'package.json') {
+      const targetDirectory = fullPath.split(path.basename(fullPath))[0]
+      const contents = fs.readdirSync(targetDirectory)
+      const has = contents.some(c => path.extname(fullPath + path.sep + c) === '.tgz')
+      if (!has) {
+        let content = 'No .tgz in ' + targetDirectory.split(cliPath)[1]
+        logContent += 'adfafa\r\n'
+        logContent += content + '\r\n'
+        errorLog(content)
+      }
+    }
+  }
+}
+
+if (logContent === '') {
+  infoLog('Packages are complete.')
+} else {
+  infoLog('A complete log of this run can be found in:')
+  infoLog('    ' + cliPath + path.sep + logFileName)
+  fs.writeFileSync('./' + logFileName, logContent)
+}
+
+function errorLog (...message) {
+  console.log('[Error]', messageHandler(...message))
+}
+function infoLog (...message) {
+  console.log('[Info', messageHandler(...message))
+}
+function messageHandler (...content) {
+  let str = ''
+  content.forEach(c => {
+    str += typeof c === 'object' ? JSON.stringify(c) : c
+    str += ' '
+  })
+  return str
+}
