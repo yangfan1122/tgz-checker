98850f7d5de0602a49432f812e367771deaaf61a
yangfan1122 <yangfan1122@gmail.com>
2022-03-11 12:16:17 +0800

    newest versison

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const packageJson = require('./package.json')
 const cliPath = process.cwd()
 let logContent = ''
+let npmScript = 'npm i ' // npm安装命令
 const timestamp = new Date().getTime()
 const logFileName = timestamp + '-tgz_debug.log'
 
@@ -20,17 +21,42 @@ check(cliPath)
 function check (directory) {
   const folders = fs.readdirSync(directory)
   for (let key in folders) {
-    const fullPath = directory + path.sep + folders[key]
+    const fullPath = directory + path.sep + folders[key] // 绝对路径
     if (fs.lstatSync(fullPath).isDirectory()) {
       check(fullPath)
     } else if (path.basename(fullPath) === 'package.json') {
+      let packageContent = fs.readFileSync(fullPath, 'utf-8') // package.jsn内容
+      packageContent = JSON.parse(packageContent)
+      const versions = Object.keys(packageContent.versions) // package.json内所有tgz的版本
+      let newest
+      const packageName = packageContent.name // 包名
+      try {
+        newest = packageContent['dist-tags']['latest']
+      } catch (e) {
+        errorLog('No "dist-tags.latest" in ' + packageName)
+        return
+      }
+      if (versions.length > 0) {
+        newest = versions[versions.length - 1] // 最新tgz的版本号
+      }
+
       const targetDirectory = fullPath.split(path.basename(fullPath))[0]
-      const contents = fs.readdirSync(targetDirectory)
-      const has = contents.some(c => path.extname(fullPath + path.sep + c) === '.tgz')
-      if (!has) {
-        let content = 'No .tgz in ' + targetDirectory.split(cliPath)[1]
-        logContent += content + '\r\n'
-        errorLog(content)
+      const contents = fs.readdirSync(targetDirectory) // 目标目录内所有文件
+      const hasTgz = contents.some(c => path.extname(fullPath + path.sep + c) === '.tgz')
+      if (!hasTgz) {
+        let tgzMessage = 'No .tgz in ' + packageName
+        logContent += tgzMessage + '\r\n'
+        npmScript += packageName + ' '
+        errorLog(tgzMessage)
+      } else {
+        const hasVersion = contents.some(c => c.indexOf(newest) > -1)
+        if (!hasVersion) {
+          // 文件里不包含最新版本
+          const versionMessage = 'No newest(' + newest + ') .tgz in ' + packageName
+          logContent += versionMessage + '\r\n'
+          npmScript += packageName + '@' + newest + ' '
+          errorLog(versionMessage)
+        }
       }
     }
   }
@@ -41,7 +67,7 @@ if (logContent === '') {
 } else {
   infoLog('A complete log of this run can be found in:')
   infoLog('    ' + cliPath + path.sep + logFileName)
-  fs.writeFileSync('./' + logFileName, logContent)
+  fs.writeFileSync('./' + logFileName, logContent + '\r\n\r\n' + npmScript)
 }
 
 function errorLog (...message) {
